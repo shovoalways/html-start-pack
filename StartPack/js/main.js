3f46a776661cf1640d5d2a4fee9a4f3fd05920d2
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2022-08-21 21:30:17 +0600

    Add files via upload

---
+++ StartPack/js/main.js
@@ -0,0 +1,22 @@
+//SlickNav Active Code;
+jQuery(document).ready(function(){
+    jQuery('#nav').slicknav();
+    jQuery.scrollUp();
+});
+
+
+//jQuery #link Animation 
+jQuery(function() {
+  jQuery('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = jQuery(this.hash);
+      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        jQuery('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
